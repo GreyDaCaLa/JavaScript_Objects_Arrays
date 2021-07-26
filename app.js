182e1a96e8cabac6f67f6d68b61f471a1f111353
GreyDaCaLa <gdele9164@gmail.com>
2021-07-25 21:48:30 -0400

    Inittial commit with fixes

--- app.js
+++ app.js
@@ -3,7 +3,41 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2,22,12,17,18,39,129];
+const arraySum = (numbers) => {
+    let sum =0;
+    numbers.forEach(element => {
+        sum = sum + element;
+        
+    });
+    return sum;
+}
+
+console.log(arraySum(numbers));
+
+
+
+
+
+
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+
+
+let book = {
+    title: "I am Number 4 by Pitticus Lore",
+    pages: 126,
+    readCount: 2,
+    info: function(){
+        return `${this.title}, ${this.pages} pages, read ${this.readCount} times.`;
+    }
+
+    
+};
+
+
+
+console.log(book.info());
 
